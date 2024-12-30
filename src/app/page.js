ff7da76cb22b541c5b70d9a55a707f5aa60f71ec
Guillermo Cerviño Porto <144793938+GuillermoGCP@users.noreply.github.com>
2024-12-30 15:14:50 +0100

    Añadir menú hamburguesa al header

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,5 @@
 import Header from '@/components/Header'
-import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <Header />
-    // <div>
-    //   <Header />
-    //   <h1>Un nuevo recurso</h1>
-    //   <p>
-    //     Organización no gubernamental que busca mejorar la calidad de vida de
-    //     las personas
-    //   </p>
-    // </div>
-  )
+  return <Header />
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,38 @@
+'use client'
 import Image from 'next/image'
 import Link from 'next/link'
 
 const Header = () => {
+  const toggleMenu = () => {}
   return (
     <header className='min-h-[14vh] flex items-center justify-between text-white p-8 bg-header border-b border-stone-300 w-[90%] m-auto font-[family-name:var(--font-geist-mono)]'>
-      <h1 className='text-3xl  text-stone-700 flex items-center justify-center flex-col '>
+      <h1 className=' text-stone-700 flex items-center justify-center flex-col sm:text-red-600 sm:text-base md:text-green-600 md:text-lg lg:text-2xl lg:text-blue-600 xl:text-3xl xl:text-yellow-600'>
         <p>Un nuevo recurso</p>
         <Image src='/logoP-NR.jpg' alt='Logo' width={100} height={100} />
       </h1>
-      <Image src='/logoNR.jpg' alt='Logo' width={500} height={500} />
+      <div className='hidden sm:block lg:width-1/2'>
+        <Image src='/logoNR.jpg' alt='Logo' width={500} height={500} />
+      </div>
+
       <nav className='text-stone-700'>
-        <ul className='flex space-x-4'>
+        {/* Menú de hamburguesa para móviles */}
+        <button onClick={toggleMenu} className='md:hidden'>
+          <svg
+            className='icon-menu'
+            xmlns='http://www.w3.org/2000/svg'
+            width='16'
+            height='16'
+            fill='currentColor'
+            viewBox='0 0 16 16'
+          >
+            <path
+              fillRule='evenodd'
+              d='M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5'
+            />
+          </svg>{' '}
+        </button>
+
+        <ul className=' space-x-4 hidden md:flex'>
           <li>
             <Link href='/'>Inicio</Link>
           </li>
